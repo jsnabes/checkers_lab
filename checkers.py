1b60d0e7e920806ad598b8c83876219961f2b1dd
Julia Snabes <julia.snabes@gmail.com>
2023-10-11 21:42:41 -0400

    last push

--- checkers.py
+++ checkers.py
@@ -1,20 +1,22 @@
+if __name__ == "__main__":
+    print("This is not intended to be run as main.")
+
 import numpy
 from numpy import random
 from numpy import reshape
 
 # first function
 def build_board(size):
-    board = random.choice([' - ', 'Red', 'Black'], size = (size,size))
+    board = random.choice(['Empty', 'Red', 'Black'], size = (size,size))
     return(board)
 
 # second function
 def get_count(board, color):
-    return(board == color).sum()
+    return((board == color).sum())
 
 # function to change shape of board
 #def board_shape(shape):
     #shape2 = numpy.reshape(shape = (shape,shape))
 
-if __name__ == "__main__":
-    print("This is not intended to be run as main.")
+
 
--- main.py
+++ main.py
@@ -7,8 +7,18 @@ def game():
     #new_shape = checkers.board_shape(board_size)
     print(full_board)
 
+# Print how many of each square
+    print("Here's how many red squares: ")
+    red = (full_board == "Red")
+    print(checkers.get_count(full_board, red))
 
+    print("Here's how many black squares: ")
+    black = (full_board == "Black")
+    print(f"{checkers.get_count(full_board, black)}")
 
+    print("Here's how many empty squares: ")
+    empty = (full_board == "empty")
+    print(f"{checkers.get_count(full_board, empty)}")
 
 if __name__ == '__main__':
     game()
